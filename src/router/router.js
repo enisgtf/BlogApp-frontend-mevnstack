81575f5b1004fa69fe3ffb09b01f514da1f9abc1
enisgtf <enisgatfar34@gmail.com>
2023-04-10 00:47:46 +0300

    fixing refresh problem

--- src/router/router.js
+++ src/router/router.js
@@ -108,7 +108,8 @@ const routes = [
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes
+    routes,
+    fallback: true 
 })
 
 router.beforeEach((to, from, next) => {
